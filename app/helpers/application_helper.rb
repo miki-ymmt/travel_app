16c80ecd7a146b59d05828bb78ebd385f53c5770
山本美希 <yamamotomiki@yamamotikinoAir.AirPort>
2024-07-06 13:29:42 +0900

    フラッシュメッセージの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when 'notice' then 'bg-blue-300'
+    when 'alert' then 'bg-orange-200'
+    when 'error' then 'bg-yellow-200'
+    else 'bg-blue-300'
+    end
+  end
 end
