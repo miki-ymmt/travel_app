7d1f164efe901e1e7534d2df3dac4bdbe4aecbb7
山本美希 <yamamotomiki@yamamotikinoAir.AirPort>
2024-07-26 12:34:58 +0900

    add/i18n日本語化対応の実施　#27

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module Myapp
       g.test_framework false     # test ファイルを作成しない
       g.skip_routes true         # ルーティングの記述を作成しない
     end
+    config.i18n.default_locale = :ja
+    config.i18n.load_path+=Dir[Rails.root.join('config','locales','**','*.yml').to_s]
   end
 end
