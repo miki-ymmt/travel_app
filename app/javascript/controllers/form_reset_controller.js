391111bc354e2c1da95cf493a66f9a5c12bbe86e
山本美希 <yamamotomiki@yamamotikinoAir.AirPort>
2024-08-03 09:57:32 +0900

    チャットbot機能の実装 #129

---
+++ app/javascript/controllers/form_reset_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="form-reset"
+export default class extends Controller {
+  reset() {
+    this.element.reset()
+  }
+}
