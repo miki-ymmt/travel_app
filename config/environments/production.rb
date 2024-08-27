c054bb97cbabb9c825bcbf23624093c2775cf33c
山本美希 <yamamotomiki@yamamotomikinoMacBook-Air.local>
2024-08-27 04:41:12 -1000

    host修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,5 +112,6 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.hosts << "adventure-travel-starter.com"
+  config.hosts << "travel-starter.onrender.com"
 end
 
