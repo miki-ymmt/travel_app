9243711f26c4ab76b24b64c2234d847010f7a5b6
山本美希 <yamamotomiki@yamamotikinoAir.AirPort>
2024-09-12 00:22:57 +0900

    oauthコントローラーの修正

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -10,31 +10,19 @@ class OauthsController < ApplicationController
 
   def callback
     provider = auth_params[:provider]
-
-    #　既存のユーザーをプロバイダ情報を元に検索し、見つかればログイン
+    # 既存のユーザーをプロバイダ情報を元に検索し、存在すればログイン
     if (@user = login_from(auth_params[:provider]))
-      flash[:notice] = "Googleアカウントでログインしました"
+      flash[:notice] = "#{provider.titleize}アカウントでログインしました"
       redirect_to home_path
     else
       begin
-
-        #Googleアカウントのメールアドレスと一致する既存のユーザーがいるか検索
-        sorcery_fetch_user_hash(provider)
-        existing_user = User.find_by(email: @user_hash[:user_info][:email])
-
-        if existing_user
-        # 既存ユーザーにGoogleプロバイダ情報を追加
-          existing_user.authentications.create(provider: provider, uid: @user_hash[:uid].to_s)
-          auto_login(existing_user)
-          redirect_to home_path, notice: 'Googleアカウントでログインしました'
-        else
-          # 新規ユーザーを作成し、Googleプロバイダ情報を追加
-          sign_up_and_login_from(provider)
-          redirect_to home_path, notice: 'Googleアカウントでログインしました'
-        end
-
-      rescue StandardError => e
-        redirect_to login_path, alert: 'Google認証に失敗しました'
+        # ユーザーが存在しない場合はプロバイダ情報を元に新規ユーザーを作成し、ログイン
+        signup_and_login(provider)
+        flash[:notice] = "#{provider.titleize}アカウントでログインしました"
+        redirect_to home_path
+      rescue StandardError
+        flash[:alert] = "#{provider.titleize}アカウントでのログインに失敗しました"
+        redirect_to login_path
       end
     end
   end
@@ -45,7 +33,7 @@ class OauthsController < ApplicationController
     params.permit(:code, :provider)
   end
 
-  def sign_up_and_login_from(provider)
+  def signup_and_login(provider)
     @user = create_from(provider)
     reset_session
     auto_login(@user)
