971ec776704398c5b610071afc7a70c8130c9eb9
山本美希 <yamamotomiki@yamamotikinoAir.AirPort>
2024-09-29 22:39:54 +0900

    テストコード追加

--- app/models/passport.rb
+++ app/models/passport.rb
@@ -3,4 +3,6 @@
 class Passport < ApplicationRecord
   belongs_to :user
   mount_uploader :photo, PhotoUploader
+
+  validates :photo, presence: true
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,8 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.include Sorcery::TestHelpers::Rails::Controller, type: :controller
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
